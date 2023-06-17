37fd565992a202f1a1068c58c4771d2306439773
po3rdlp <leonpolandos53@gmail.com>
2023-06-17 18:52:51 +0800

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NavigationBar />
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import NavigationBar from './components/Molecules/NavigationBar.vue';
+
+</script>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1 class=" text-xs">Hello this is Home pages</h1>
+    </div>
+  </main>
+</template>
